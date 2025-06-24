c2affae0533b3c36ffee6bca5a064f687e241650
Alexandre-Passos <alexandre15passos@gmail.com>
2025-06-24 09:10:29 -0400

    15

---
+++ atividade-15.js
@@ -0,0 +1,11 @@
+/*
+Você está criando um aplicativo para exibir a sequência de números até um valor que o usuário forneceu. 
+O objetivo é mostrar todos os números a partir de 1 até o valor informado pelo usuário, de forma ordenada e crescente.
+
+Como você pode gerar essa contagem e garantir que cada número seja mostrado até atingir o valor final fornecido?
+*/
+
+const numeroFornecido = 27
+for (let i = 1; i <= numeroFornecido; i++){
+    console.log(i)
+}
