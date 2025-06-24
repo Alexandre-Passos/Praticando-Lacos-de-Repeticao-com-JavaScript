8b4d0fdfed0e3b2a261d72b85fdf90e6ac613cbe
Alexandre-Passos <alexandre15passos@gmail.com>
2025-06-24 09:32:25 -0400

    17

---
+++ atividade-17.js
@@ -0,0 +1,15 @@
+/*
+Você está criando um sistema de análise para um cliente que precisa identificar todos os 
+números pares dentro de um intervalo informado. Ele quer saber quais valores 
+são múltiplos de 2, para aplicar um filtro em dados financeiros.
+
+Crie um programa que exiba todos os números pares de 1 até o número final fornecido pelo usuário.
+*/
+
+const informarNumero = 21
+
+for (let i = 1; i <= informarNumero; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
